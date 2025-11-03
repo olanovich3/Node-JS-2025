723966bc933a81e95b4782882aac086d3165f4df
Olano <colano@wktksoft.com>
2025-11-03 16:08:20 +0100

    Add new endpoint

--- src/framework/server.ts
+++ src/framework/server.ts
@@ -1,9 +1,12 @@
-process.loadEnvFile()
-import express from 'express'
-const app = express()
-const PORT = process.env.PORT
-app.get('/hello', (request, res, nextFunction) => {
-    res.status(200).send({ message:'Hello World endpoint recibido!'})
-  })
+process.loadEnvFile();
+import express from "express";
+const app = express();
+const PORT = process.env.PORT;
+app.get("/hello", (request, res, nextFunction) => {
+  res.status(200).send({ message: "Hello World endpoint recibido!" });
+});
+app.get("/oldfeature", (request, res, nextFunction) => {
+  res.status(503).send({ message: "Servicio deshabilitado" });
+});
 
-  export default app
+export default app;
